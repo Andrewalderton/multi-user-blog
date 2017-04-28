5ef0c125f4423c120a0f64dd42188deb71844f76
Andy Alderton <andrewalderton@sky.com>
2017-04-28 20:45:24 +0100

    Fix Post render function, lint python

--- main.py
+++ main.py
@@ -22,56 +22,68 @@ import hmac
 from string import letters
 
 import jinja2
-
-import sys
-
 import webapp2
 
 from google.appengine.ext import db
 
-template_dir = os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                                autoescape=True)
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
 
-secret = "ghudhh.sixi6,skja$ty12DSzo~aplrjvbd"
+secret = "ghudhhsixi6skja$ty12DSzoaplrjvbd"
+
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+
+def users_key(group='default'):
+    return db.Key.from_path('users', group)
+
+
+def blog_key(name='default'):
+    return db.Key.from_path('blogs', name)
+
+
+# Authentication
 def make_secure_val(val):
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
 
 def check_secure_val(secure_val):
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
 
-def users_key(group='default'):
-    return db.Key.from_path('users', group)
 
-def blog_key(name='default'):
-    return db.Key.from_path('blogs', name)
-
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-def valid_username(username):
-    return username and USER_RE.match(username)
-
-PASS_RE = re.compile(r"^.{3,20}$")
-def valid_password(password):
-    return password and PASS_RE.match(password)
-
-def make_salt(length = 5):
+def make_salt(length=5):
     return ''.join(random.choice(letters) for x in xrange(length))
 
-def make_pw_hash(name, pw, salt=None):
+
+def make_pw_hash(username, password, salt=None):
     if not salt:
-        h = hashlib.sha256(name + pw + salt).hexdigest()
+        salt = make_salt()
+        h = hashlib.sha256(username + password + salt).hexdigest()
         return '%s,%s' % (salt, h)
+
 
 def valid_pw(name, password, h):
     salt = h.split(',')[0]
     return h == make_pw_hash(name, password, salt)
+
+
+# Username and password validation
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+def valid_password(password):
+    return password and PASS_RE.match(password)
 
 
 class Handler(webapp2.RequestHandler):
@@ -80,8 +92,7 @@ class Handler(webapp2.RequestHandler):
 
     def render_str(self, template, **params):
         params["user"] = self.user
-        t = jinja_env.get_template(template)
-        return t.render(params)
+        return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
@@ -115,73 +126,25 @@ class User(db.Model):
 
     @classmethod
     def by_id(cls, uid):
-        return cls.get_by_id(uid, parent=users_key())
+        return User.get_by_id(uid, parent=users_key())
 
     @classmethod
     def by_name(cls, name):
-        u = cls.all().filter('name =', name).get()
+        u = User.all().filter('name =', name).get()
         return u
 
     @classmethod
-    def register(cls, name, pw):
-        pw_hash = make_pw_hash(name, pw)
-        return cls(parent=users_key(),
-        name=name,
-        pw_hash = pw_hash,
-        )
+    def register(cls, username, password):
+        pw_hash = make_pw_hash(username, password)
+        return User(parent=users_key(),
+                    name=username,
+                    pw_hash=pw_hash)
 
     @classmethod
-    def login(cls, name, pw):
-        u = cls.by_name(name)
-        if u and valid_pw(name, pw, u.pw_hash):
+    def login(cls, username, password):
+        u = cls.by_name(username)
+        if u and valid_pw(username, password, u.pw_hash):
             return u
-
-
-class MainPage(Handler):
-    def get(self):
-        # posts = self.request.get_all("posts")
-        posts = Post.all().order('-created')
-        self.render("front.html", posts=posts)
-
-
-class Post(db.Model):
-    title = db.StringProperty(required=True)
-    body = db.TextProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
-
-    def render(self, title, body, created):
-        self._render_text = self.body.replace('\n', '<br>')
-        self.render("post.html", title, body, created)
-
-
-class PostPage(Handler):
-    def get(self, post_id):
-        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-        post = db.get(key)
-
-        if not post:
-            self.error(404)
-            return
-
-        self.render("permalink.html", post=post)
-
-
-class NewPost(Handler):
-    def get(self):
-        self.render("new-post.html")
-
-    def post(self):
-        title = self.request.get("title")
-        body = self.request.get("body")
-
-        if title and body:
-            post = Post(parent=blog_key(), title=title, body=body)
-            post.put()
-            self.redirect('/blog/%s' % str(post.key().id()))
-        else:
-            error = "Title and content, please!"
-            self.render("new-post.html", title=title, body=body, error=error)
 
 
 class SignUp(Handler):
@@ -207,9 +170,8 @@ class SignUp(Handler):
         self.password = self.request.get("password")
         self.verify = self.request.get("verify")
 
-        params = dict(username=self.username)
+        params = dict(username=self.username, )
 
-        print(self.username)
         if not valid_username(self.username):
             params["error_username"] = "That is not a valid username."
             have_error = True
@@ -217,21 +179,63 @@ class SignUp(Handler):
         if not valid_password(self.password):
             params["error_password"] = "That wasn't a valid password."
             have_error = True
+
         elif self.password != self.verify:
             params["error_verify"] = "Your passwords didn't match."
+            have_error = True
 
         if have_error:
             self.render("sign-up.html", **params)
         else:
             self.done()
 
-    # def done(self, *a, **kw):
-    #     raise NotImplementedError
+
+class MainPage(Handler):
+    def get(self):
+        # posts = self.request.get_all("posts")
+        posts = Post.all().order('-created')
+        self.render("front.html", posts=posts)
 
 
-# class Unit2Signup(SignUp):
-#     def done(self):
-#         self.redirect('/unit2/welcome?username=' + self.username)
+class Post(db.Model):
+    title = db.StringProperty(required=True)
+    body = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    def render(self, title, body, created):
+        self._render_text = self.body.replace('\n', '<br>')
+        return render_str("post.html", post=self, title=title,
+                          body=body, created=created)
+
+
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post=post)
+
+
+class NewPost(Handler):
+    def get(self):
+        self.render("new-post.html")
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            post = Post(parent=blog_key(), title=title, body=body)
+            post.put()
+            self.redirect('/%s' % str(post.key().id()))
+        else:
+            error = "Title and content, please!"
+            self.render("new-post.html", title=title, body=body, error=error)
 
 
 class LoginPage(Handler):
@@ -245,23 +249,10 @@ class LoginPage(Handler):
         u = User.login(username, password)
         if u:
             self.login(u)
-            self.redirect('/unit3/welcome')
+            self.redirect('/welcome')
         else:
             msg = 'Invalid login'
-            self.render('login.html', error = msg)
-
-
-# class Rot13(Handler):
-#     def get(self):
-#         self.render("rot13.html")
-
-#     def post(self):
-#         rot13 = ''
-#         text = self.request.get("text")
-#         if text:
-#             rot13 = text.encode("rot13")
-
-#         self.render("rot13.html", text=rot13)
+            self.render('login.html', error=msg)
 
 
 class LogOut(Handler):
@@ -269,13 +260,6 @@ class LogOut(Handler):
         self.logout()
         self.redirect('/sign-up')
 
-
-# class Unit3Welcome(Handler):
-#     def get(self):
-#         if self.user:
-#             self.render('welcome.html', username=self.username)
-#         else:
-#             self.redirect('/sign-up')
 
 class Welcome(Handler):
     def get(self):
@@ -288,12 +272,10 @@ class Welcome(Handler):
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    # ('/unit2/rot13', Rot13),
     ('/login', LoginPage),
+    ('/log-out', LogOut),
     ('/sign-up', SignUp),
     ('/welcome', Welcome),
-    # ('/unit3/welcome', Unit3Welcome),
-    ('/blog/new-post', NewPost),
-    ('/blog/([0-9]+)', PostPage),
-    ('/log-out', LogOut)
+    ('/new-post', NewPost),
+    ('/([0-9]+)', PostPage)
 ], debug=True)
